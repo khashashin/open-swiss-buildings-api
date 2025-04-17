e9458a6c21eb7cf4f4714bdef1c8432302846c39
Yusuf Khasbulatov <khashashin@gmail.com>
2025-04-17 15:47:49 +0200

    Fix permissions for cache, log, and data directories in entrypoint script

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,6 +3,9 @@ set -e
 
 export APP_ENV=${APP_ENV:-prod}
 
+chown -R php:php /www/var/cache /www/var/log /www/var/data
+chmod -R u+rwX /www/var/cache /www/var/log /www/var/data
+
 echo "Clearing Symfony cache..."
 php bin/console cache:clear --env="$APP_ENV"
 
